69aeb87cfeb48cb9f012d0082f9f2bf9f60cf6c0
cnaegele <cnaegele@lausanne.ch>
2026-04-17 12:59:49 +0200

    parget du form dans un iframe

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,79 @@
 <template>
+  <div v-if="messageerreur !== ''">{{ messageerreur }}</div>
   <iframe name="printFrame" style="width: 100%; height: 100vh; border: none;" />
   <form ref="formRef" method="POST" action="https://print-vdl-test.lausanne.ch/wsprint-v1.6/print/post"
-    target="printFrame" style="display: none;">
-    <input type="hidden" name="application" value="GOELAND" />
-    <input type="hidden" name="document" :value="'print' + siddoc" />
-    <input type="hidden" name="format" value="7" />
-    <input type="hidden" name="fluxXML64" :value="dataxml64enc" />
+    target="printFrame" >
+    <input ref="applicationRef" type="hidden" name="application" value="GOELAND" />
+    <input ref="documentRef" type="hidden" name="document" :value="'print' + siddoc" />
+    <input ref="formatRef" type="hidden" name="format" value="7" />
+    <input ref="fluxXML64Ref" type="hidden" name="fluxXML64" :value="dataxml64enc" />
   </form>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted, nextTick } from 'vue'
 import axios from 'axios'
-import type { AxiosResponse, AxiosError } from 'axios'
+import type { AxiosResponse } from 'axios'
 
+const messageerreur = ref<string>('')
 const urlParams = new URLSearchParams(window.location.search)
 let idaffaire: number | null = null
+let contexte: string | null = null
 const prmsidaffaire = urlParams.get('idaffaire')
 if (prmsidaffaire !== null && prmsidaffaire !== '') {
   idaffaire = stringToPositiveInteger(prmsidaffaire)
 }
-const siddoc = ref<string>('OPCDispenseAutorisation')
-const dataxml64enc = ref<string>('')
-const formRef = ref<HTMLFormElement | null>(null)
-onMounted(async () => {
-  if (idaffaire !== null) {
-    console.log(`idaffaire : ${idaffaire}`)
+const prmscontexte = urlParams.get('contexte')
+if (prmscontexte !== null && prmscontexte !== '') {
+  contexte = prmscontexte
+}
+let bprmsok = true
+let siddoccontexte: string = ''
+let pagecontexte: string = ''
+switch (contexte) {
+  case 'afft327lettredispenseautorisation':
+    siddoccontexte = 'OPCDispenseAutorisation'
+    pagecontexte = '/goeland/bdocsoi/axios/afft327lettredispenseautorisation.php'
+    break;
+  default:
+    messageerreur.value += 'parametre "contexte" manquant ou invalide. '
+    bprmsok = false
+}
+if (idaffaire === null) {
+  messageerreur.value += 'parametre "idaffaire" manquant ou invalide. '
+  bprmsok = false
+}
+console.log(`idaffaire : ${idaffaire}`)
+console.log(`contexte : ${contexte}`)
+console.log(`bprmsok : ${bprmsok}`)
+console.log(`messageerreur : ${messageerreur.value}`)
 
+const siddoc = ref<string>('')
+const dataxml64enc = ref<string>('')
+siddoc.value = siddoccontexte
+const formRef = ref<HTMLFormElement | null>(null)
+const applicationRef = ref<HTMLFormElement | null>(null)    
+const documentRef = ref<HTMLFormElement | null>(null)    
+const formatRef = ref<HTMLFormElement | null>(null)    
+const fluxXML64Ref = ref<HTMLFormElement | null>(null)    
+
+onMounted(async () => {
+  if (bprmsok) {
     let server: string = ''
     if (import.meta.env.DEV) {
       server = 'https://mygolux.lausanne.ch'
     }
-    const page: string = '/goeland/bdocsoi/axios/afft327lettredispenseautorisation.php'
+    const page: string = pagecontexte
     const urlaffdl: string = `${server}${page}`
-    const params = new URLSearchParams([['idaffaire', idaffaire.toString()]])
+    const params = new URLSearchParams([['idaffaire', (idaffaire ?? 0).toString()]])
     const response: AxiosResponse = await axios.get(urlaffdl, { params })
     console.log(response.data)
     dataxml64enc.value = response.data
     await nextTick()
+    console.log("input applicationRef contient", applicationRef.value)
+    console.log("input documentRef contient", documentRef.value)
+    console.log("input formatRef contient", formatRef.value)
+    console.log("input fluxXML64 contient", fluxXML64Ref.value)
     formRef.value?.submit()
   }
 })
